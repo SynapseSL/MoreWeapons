b6988ec7c2e02f0378f7634d868ff21d1deb138f
GrafDimenzio <crafterwolfhd@gmail.com>
2020-11-26 14:56:18 +0100

    update version

--- MoreWeapons/PluginClass.cs
+++ MoreWeapons/PluginClass.cs
@@ -10,7 +10,7 @@ namespace MoreWeapons
         SynapseMajor = SynapseController.SynapseMajor,
         SynapseMinor = SynapseController.SynapseMinor,
         SynapsePatch = SynapseController.SynapsePatch,
-        Version = "1.0.2"
+        Version = "1.1.0"
         )]
     public class PluginClass : AbstractPlugin
     {

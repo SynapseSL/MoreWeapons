bb0932a2010376aae13e1ed04d2a37279a79a719
TheVoidNebula <75329526+TheVoidNebula@users.noreply.github.com>
2021-07-14 20:47:08 +0200

    Update EventHandlers.cs

--- MoreWeapons/EventHandlers.cs
+++ MoreWeapons/EventHandlers.cs
@@ -5,12 +5,22 @@ namespace MoreWeapons
 {
     public class EventHandlers
     {
-        public EventHandlers() => Server.Get.Events.Player.PlayerPickUpItemEvent += Pickup;
+        public EventHandlers()
+        {
+            Server.Get.Events.Player.PlayerPickUpItemEvent += Pickup;
+            Server.Get.Events.Player.PlayerChangeItemEvent += Equip;
+        }
 
         private void Pickup(Synapse.Api.Events.SynapseEventArguments.PlayerPickUpItemEventArgs ev)
         {
             if (System.Enum.GetValues(typeof(CustomItemType)).ToArray<CustomItemType>().Any(x => (int)x == ev.Item.ID))
                 ev.Player.GiveTextHint($"You have picked up a {(CustomItemType)ev.Item.ID}");
+        }
+        
+         private void Equip(Synapse.Api.Events.SynapseEventArguments.PlayerChangeItemEventArgs ev)
+        {
+            if (System.Enum.GetValues(typeof(CustomItemType)).ToArray<CustomItemType>().Any(x => (int)x == ev.NewItem.ID))
+                ev.Player.GiveTextHint($"You have equipped {(CustomItemType)ev.NewItem.ID}");
         }
     }
 }
